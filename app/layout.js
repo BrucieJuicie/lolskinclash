a299e9812feba3907b7e994a38ca26e0d57914a6
Bruce <bruceogden@rocketmail.com>
2025-04-12 16:18:47 -0500

    Adjusting formatting

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
       </head>
       <body className="bg-background text-foreground font-sans">
         <Header />
-        <main className="min-h-screen flex flex-col items-center justify-center p-4">
+        <main className="min-h-screen flex flex-col items-center p-4">
           {children}
         </main>
         <Footer />
